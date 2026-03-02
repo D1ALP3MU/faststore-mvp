106c5b9215e9adeb2eae1f70947a1ed0f5962419
D1ALP3MU <diegoalejandroperezm@gmail.com>
2026-03-02 14:41:42 -0500

    feat: Deshabilito UseHttpsRedirection para pruebas locales con Angular

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); // Deshabilitado para pruebas locales con Angular
 
 app.MapControllers();
 
